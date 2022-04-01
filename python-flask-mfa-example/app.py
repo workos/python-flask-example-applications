562d0dc06bca2fe686c3b3dcd8c89682adb8ea5c
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2022-04-01 16:57:17 -0600

    Add SMS MFA workflow and routing

--- python-flask-mfa-example/app.py
+++ python-flask-mfa-example/app.py
@@ -16,7 +16,7 @@ workos.base_api_url = 'http://localhost:7000/' if DEBUG else workos.base_api_url
 
 
 @app.route('/')
-def login():
+def home():
     print(session)
     if session['factor_list']:
         return render_template('list_factors.html', factors=session['factor_list'])
@@ -37,3 +37,40 @@ def enroll_sms_factor():
     print("this is factor list before redirect", session['factor_list'])
     session.modified = True
     return redirect('/')
+
+
+@app.route('/factor_detail')
+def factor_detail():
+    factorId = request.args.get('id')
+    for factor in session['factor_list']:
+        if factor['id'] == factorId:
+            fullFactor = factor
+    session['current_factor'] = fullFactor["id"]
+    session.modified = True
+    return render_template('factor_detail.html', factor=fullFactor)
+
+
+@app.route('/challenge_factor', methods=["POST"])
+def challenge_factor():
+    message = request.form['sms_message']
+    session['sms_message'] = message
+
+    challenge = workos.client.mfa.challenge_factor(
+        authentication_factor_id=session['current_factor'],
+        sms_template=message,
+    )
+    session['challenge_id'] = challenge['id']
+    session.modified = True
+    return render_template('challenge_factor.html')
+
+
+@app.route('/verify_factor', methods=["POST"])
+def verify_factor():
+    code = request.form['code']
+    challenge_id = session['challenge_id']
+    verify_factor = workos.client.mfa.verify_factor(
+        authentication_challenge_id=challenge_id,
+        code=code,
+    )
+    print(verify_factor['challenge'])
+    return render_template('challenge_success.html', challenge=verify_factor['challenge'])
