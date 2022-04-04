9ee1476b447cf21bf056ff91d0b3d870d447af55
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2022-04-04 16:12:01 -0600

    Update enroll factor flow to include totp

--- python-flask-mfa-example/app.py
+++ python-flask-mfa-example/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import (Flask, session, redirect, render_template, request, url_for)
+import json
 import workos
 
 
@@ -17,24 +18,41 @@ workos.base_api_url = 'http://localhost:7000/' if DEBUG else workos.base_api_url
 
 @app.route('/')
 def home():
-    print(session)
+    # print(session)
     if session['factor_list']:
         return render_template('list_factors.html', factors=session['factor_list'])
-    return render_template('list_factors.html')
+    return render_template('list_factors.html', )
 
 
-@app.route('/enroll_sms_factor')
-def enroll_sms_factor():
-    print('hit enroll new factor')
-    new_factor = workos.client.mfa.enroll_factor(
-        type='sms',
-        phone_number='9204703484'
-    )
+@app.route('/enroll_factor_details', methods=["GET"])
+def enroll_factor_details():
+    return render_template('enroll_factor.html')
+
+
+@app.route('/enroll_factor', methods=["POST"])
+def enroll_factor():
+    factor_type = request.form.get('type')
+    totp_issuer = request.form.get('totp_issuer')
+    totp_user = request.form.get('totp_user')
+    phone_number = request.form.get('phone_number')
+
+    if factor_type == "sms":
+        factor_type = "sms"
+        new_factor = workos.client.mfa.enroll_factor(
+            type=factor_type,
+            phone_number=phone_number
+        )
+
+    if factor_type == "totp":
+        factor_type = "totp"
+        new_factor = workos.client.mfa.enroll_factor(
+            type=factor_type,
+            totp_issuer=totp_issuer,
+            totp_user=totp_user
+        )
+
     print(new_factor)
-    # will need to have logic to ADD the new factor to the list
-
     session['factor_list'].append(new_factor)
-    print("this is factor list before redirect", session['factor_list'])
     session.modified = True
     return redirect('/')
 
@@ -74,3 +92,9 @@ def verify_factor():
     )
     print(verify_factor['challenge'])
     return render_template('challenge_success.html', challenge=verify_factor['challenge'])
+
+
+@app.route('/clear_session', methods=["GET"])
+def clear_session():
+    session["factor_list"] = []
+    return redirect('/')
