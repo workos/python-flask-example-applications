ebdbc0b2136fc7796cc6cb42d79fdeb0634cdb7b
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2022-06-01 15:37:37 -0600

    Make change to trigger PR refresh

--- python-flask-directory-sync-example/app.py
+++ python-flask-directory-sync-example/app.py
@@ -1,8 +1,8 @@
 import os
-from flask import (Flask, render_template, request)
+from flask import Flask, render_template, request
 import workos
 from workos import client as workos_client
-from flask_socketio import (SocketIO, emit)
+from flask_socketio import SocketIO, emit
 import json
 
 
@@ -10,63 +10,64 @@ DEBUG = False
 app = Flask(__name__)
 
 
-app.config['SECRET_KEY'] = 'secret!'
+app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app)
 
-workos.api_key = os.getenv('WORKOS_API_KEY')
-workos.base_api_url = 'http://localhost:5000/' if DEBUG else workos.base_api_url
-directory_id = os.getenv('DIRECTORY_ID')
+workos.api_key = os.getenv("WORKOS_API_KEY")
+workos.base_api_url = "http://localhost:5000/" if DEBUG else workos.base_api_url
+directory_id = os.getenv("DIRECTORY_ID")
 
-@app.route('/')
+
+@app.route("/")
 def home():
     directories = workos.client.directory_sync.list_directories()
     print(directories)
     directoryNames = []
-    for i in directories['data']:
-        directoryNames.append(i['name'])
+    for i in directories["data"]:
+        directoryNames.append(i["name"])
     print(directoryNames)
-    return render_template('home.html', directories=directories)
+    return render_template("home.html", directories=directories)
 
-@app.route('/directory')
+
+@app.route("/directory")
 def directory():
-    directory_id = request.args.get('id')
+    directory_id = request.args.get("id")
     print(directory_id)
     directory = workos.client.directory_sync.get_directory(directory_id)
     print(directory)
-    return render_template('directory.html', directory=directory, id=directory['id'])
+    return render_template("directory.html", directory=directory, id=directory["id"])
 
-@app.route('/users')
+
+@app.route("/users")
 def directory_users():
-    directory_id = request.args.get('id')
+    directory_id = request.args.get("id")
     users = workos_client.directory_sync.list_users(directory=directory_id)
-    return render_template('users.html', users=users)
+    return render_template("users.html", users=users)
 
 
-@app.route('/groups')
+@app.route("/groups")
 def directory_groups():
-    directory_id = request.args.get('id')
+    directory_id = request.args.get("id")
     groups = workos_client.directory_sync.list_groups(directory=directory_id)
 
-    return render_template('groups.html', groups=groups)
+    return render_template("groups.html", groups=groups)
 
 
-@app.route('/webhooks', methods=['GET', 'POST'])
+@app.route("/webhooks", methods=["GET", "POST"])
 def webhooks():
     print(request)
     if request.data:
         payload = request.get_data()
-        sig_header = request.headers['WorkOS-Signature']
+        sig_header = request.headers["WorkOS-Signature"]
         response = workos_client.webhooks.verify_event(
-            payload = payload,
-            sig_header = sig_header,
-            secret = os.getenv('WEBHOOKS_SECRET')
+            payload=payload, sig_header=sig_header, secret=os.getenv("WEBHOOKS_SECRET")
         )
-        
+
         message = json.dumps(response)
-        socketio.emit('webhook_received', message)
+        socketio.emit("webhook_received", message)
 
     # Return a 200 to prevent retries based on validation
-    return render_template('webhooks.html')
+    return render_template("webhooks.html")
--- python-flask-magic-link-example/app.py
+++ python-flask-magic-link-example/app.py
@@ -1,52 +1,56 @@
 import os
-from flask import (Flask, redirect, render_template, request, url_for)
+from flask import Flask, redirect, render_template, request, url_for
 import workos
 from workos import client as workos_client
-# Flask Setup 
+
+# Flask Setup
 DEBUG = False
 app = Flask(__name__)
 
 # WorkOS Setup
-workos.api_key = os.getenv('WORKOS_API_KEY')
-workos.project_id = os.getenv('WORKOS_CLIENT_ID')
-workos.base_api_url = 'http://localhost:5000/' if DEBUG else workos.base_api_url
+workos.api_key = os.getenv("WORKOS_API_KEY")
+workos.project_id = os.getenv("WORKOS_CLIENT_ID")
+workos.base_api_url = "http://localhost:5000/" if DEBUG else workos.base_api_url
+
 
 @app.route("/")
 def hello_world():
-    return render_template('login.html')
+    return render_template("login.html")
 
 
-@app.route('/callback')
+@app.route("/callback")
 def callback():
-    code = request.args.get('code')
+    code = request.args.get("code")
     profile_and_token = workos_client.sso.get_profile_and_token(code)
 
     # Use the information in `profile` for further business logic.
     profile = profile_and_token.profile
 
-    return redirect('/')
+    return redirect("/")
 
 
-@app.route('/passwordless_auth', methods=["POST"])
+@app.route("/passwordless_auth", methods=["POST"])
 def passwordless_auth():
-    email = request.form['email']
+    email = request.form["email"]
 
     session = workos_client.passwordless.create_session(
-      {'email': email, 'type': 'MagicLink'}
+        {"email": email, "type": "MagicLink"}
     )
 
     # Send a custom email using your own service
-    print(email, session['link'])
+    print(email, session["link"])
 
     # Finally, redirect to a "Check your email" page
-    return render_template('serve_magic_link.html', email=email, magic_link=session['link'])
+    return render_template(
+        "serve_magic_link.html", email=email, magic_link=session["link"]
+    )
 
 
-@app.route('/success')
+@app.route("/success")
 def success():
-    code = request.args.get('code')
+    code = request.args.get("code")
     profile = workos.client.sso.get_profile_and_token(code)
     p_profile = profile.to_dict()
-    raw_profile = p_profile['profile']
+    raw_profile = p_profile["profile"]
 
-    return render_template('success.html', raw_profile=raw_profile)
+    return render_template("success.html", raw_profile=raw_profile)
