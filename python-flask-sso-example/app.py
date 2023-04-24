31a2891ca4dc9559b64a61b111beed4e8e8d8bde
Adam Wolfman <47123183+awolfden@users.noreply.github.com>
2023-04-24 12:40:26 -0600

    Update UI/UX for Flask SSO app (#38)
    * Update UI/UX for flask SSO app
    
    * Remove print statements
    
    * Update readme
    
    ---------
    
    Co-authored-by: Adam Wolfman <adamwolfman@Adams-MBP-3.domain>

--- python-flask-sso-example/app.py
+++ python-flask-sso-example/app.py
@@ -16,9 +16,9 @@ workos.api_key = os.getenv("WORKOS_API_KEY")
 workos.project_id = os.getenv("WORKOS_CLIENT_ID")
 workos.base_api_url = "http://localhost:7000/" if DEBUG else workos.base_api_url
 
-# Enter Connection ID here
+# Enter Organization ID here
 
-CUSTOMER_CONNECTION_ID = ""
+CUSTOMER_ORGANIZATION_ID = ""
 
 
 def to_pretty_json(value):
@@ -40,14 +40,19 @@ def login():
         return render_template("login.html")
 
 
-@app.route("/auth")
+@app.route("/auth", methods=["POST"])
 def auth():
 
-    authorization_url = workos.client.sso.get_authorization_url(
-        redirect_uri=url_for("auth_callback", _external=True),
-        state={},
-        connection=CUSTOMER_CONNECTION_ID,
-    )
+    login_type = request.form.get("login_method")
+
+    params = {"redirect_uri": url_for("auth_callback", _external=True), "state": {}}
+
+    if login_type == "saml":        
+        params["organization"] = CUSTOMER_ORGANIZATION_ID
+    else:
+        params["provider"] = login_type
+
+    authorization_url = workos.client.sso.get_authorization_url(**params)
 
     return redirect(authorization_url)
 
